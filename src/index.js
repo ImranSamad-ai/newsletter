aa3aabd109fcc3d95cb65fb39ae2f79a20ac6d21
Abdussamad <imranabdussamad35@gmail.com>
2025-10-29 21:37:53 +0100

    Enable CORS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const subscriberRoute = require("./routes/subscriberRoute");
+
+app.use(
+  cors({
+    origin: "http://localhost:5173", // or use "*" to allow all
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use("/subc", subscriberRoute);
