46d12d9b40ebfc35509b45aeffb19242ef369843
Abdussamad <imranabdussamad35@gmail.com>
2026-01-01 08:33:50 +0100

    implementing the compleedt

--- src/db.js
+++ src/db.js
@@ -2,12 +2,11 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     mongoose.connect(
-      "mongodb+srv://wdbUser:SVvNP.wNiXk-rf2@cluster0.jikdcsc.mongodb.net/"
+      "mongodb+srv://wdbUser:SVvNP.wNiXk-rf2@cluster0.jikdcsc.mongodb.net"
     );
     console.log("now connected to the server");
   } catch (error) {
     console.log(error);
   }
 };
-
 module.exports = connectDB;
