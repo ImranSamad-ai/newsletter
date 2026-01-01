823d9ec6a84030ca0bfc7a9abdf8e3a3ab8eacca
Abdussamad <imranabdussamad35@gmail.com>
2026-01-01 08:38:58 +0100

    implementing the compledt

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const connectDB = require("./src/db");
 const app = require("./src/index");
 
