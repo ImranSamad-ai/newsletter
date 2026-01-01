76b505ebd057a3aa0f199fad60013ebd125fbb54
Abdussamad <imranabdussamad35@gmail.com>
2026-01-01 01:11:22 +0100

    implementing the completed feature

--- src/controllers/reminderController.js
+++ src/controllers/reminderController.js
@@ -32,3 +32,46 @@ exports.getReminders = async (req, res) => {
     res.status(400).json({ message: "failed", error });
   }
 };
+exports.editReminder = async (req, res) => {
+  try {
+    const user = req.user._id;
+
+    const reminders = await reminderModel.findByIdAndUpdate(
+      user,
+      {
+        priority: req.body.priority,
+        remindAt: req.body.remindAt,
+        message: req.body.message,
+        title: req.body.title,
+        channel: req.body.channel,
+        contact: req.body.contact,
+      },
+      { new: true }
+    );
+
+    res.status(200).json({
+      reminders,
+      message: "Contact reminder",
+    });
+  } catch (error) {
+    res.status(400).json({ message: "failed", error });
+  }
+};
+exports.done = async (req, res) => {
+  try {
+    const user = req.body._id;
+
+    const reminders = await reminderModel.findByIdAndUpdate(
+      user,
+      { completed: req.body.completed },
+      { new: true }
+    );
+
+    res.status(200).json({
+      reminders,
+      message: "Contact reminder",
+    });
+  } catch (error) {
+    res.status(400).json({ message: "failed", error });
+  }
+};
--- src/routes/reminderRoute.js
+++ src/routes/reminderRoute.js
@@ -4,8 +4,15 @@ const { protect } = require("../controllers/authController");
 const {
   createReminder,
   getReminders,
+  done,
+  editReminder,
 } = require("../controllers/reminderController");
 
-router.route("/").post(protect, createReminder).get(protect, getReminders);
+router
+  .route("/")
+  .post(protect, createReminder)
+  .get(protect, getReminders)
+  .patch(protect, editReminder);
 
+router.patch("/completed", protect, done);
 module.exports = router;
