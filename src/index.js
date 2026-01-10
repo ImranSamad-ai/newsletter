3966c7c8ca0f3bd7bec7265ac5af9b3e6366c571
Abdussamad <imranabdussamad35@gmail.com>
2026-01-11 00:18:06 +0100

    implementing the acoouit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+/////////////////////
+
+const webpush = require("web-push");
+const schedule = require("node-schedule");
+const bodyParser = require("body-parser");
+
+///////////////
 const AppError = require("./utils/AppError");
 const globalErrorHandler = require("./controllers/errorController");
 const contactRoute = require("./routes/contactRoute");
@@ -14,8 +21,56 @@ const corsOptions = {
   credentials: true,
 };
 
-app.use(cors(corsOptions));
+// /////////////////////
 
+// REPLACE THESE with the keys you generated
+const publicVapidKey =
+  "BOKn7rqp9YlAyqCCfJ4cagNlam2OucC27i2DzNj2ZPH8JuELQvC3siNX6us60L3vxQqTbgNAXoOC6taBG7wO7to";
+const privateVapidKey = "t-tGHFaMbxSDbZOWnQmyDBHLPVI4s3yz6RmkWnJR3ys";
+
+webpush.setVapidDetails(
+  "mailto:imranabdussamad35@gmail.com",
+  publicVapidKey,
+  privateVapidKey
+);
+
+// In a real app, store this subscription in a database!
+let userSubscription = null;
+
+// 1. Route for the frontend to send the "Subscription Object"
+app.post("/subscribe", (req, res) => {
+  userSubscription = req.body;
+  res.status(201).json({});
+});
+
+// 2. Route to schedule a notification
+app.post("/schedule-push", (req, res) => {
+  const { time, message } = req.body; // 'time' should be a JS Date string
+
+  if (!userSubscription) {
+    return res.status(400).json({ error: "No user subscribed yet." });
+  }
+
+  console.log(`Notification scheduled for: ${time}`);
+
+  // The Magic: This triggers the push at the exact 'time' selected
+  schedule.scheduleJob(new Date("2026-01-10T23:03:58.614Z"), function () {
+    const payload = JSON.stringify({ title: "Reminder", body: message });
+
+    webpush
+      .sendNotification(userSubscription, payload)
+      .catch((err) => console.error("Push Error:", err));
+
+    console.log("Push notification sent!");
+  });
+
+  res.json({ success: true, info: `Scheduled for ${time}` });
+});
+
+// ///////////////////////////
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/auth", authRoute);
